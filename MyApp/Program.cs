4920360f0f1ef2bff6081f6839032d12f8635c61
Miguel <chanampamiguel@gmail.com>
2022-06-14 15:22:24 -0300

    Mostrar contenido de una carpeta

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Ingrese una ruta: ");
+string path = Console.ReadLine()!;
+
+mostrarDirectorio(path);
+
+void mostrarDirectorio(string path) {
+
+  if (Directory.Exists(path)) {
+      
+    List<string> listadoCarpetas = Directory.GetDirectories(path).ToList();
+
+    foreach (string carpeta in listadoCarpetas) {
+
+      string[] carpetaAMostrar = carpeta.Split("\\");
+
+      Console.WriteLine("/" + carpetaAMostrar[carpetaAMostrar.Length - 1]);
+    };
+
+    foreach (string carpeta in Directory.GetFiles(path)) {
+      
+      string[] carpetaAMostrar = carpeta.Split("\\");
+
+      Console.WriteLine(carpetaAMostrar[carpetaAMostrar.Length - 1]);
+    };
+  };
+};
