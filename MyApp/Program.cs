b22a18d6f3c99dabf3a7db16270c752f2f10a826
Miguel <chanampamiguel@gmail.com>
2022-06-18 00:09:46 -0300

    Guardar archivos de directorio en un csv

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,26 +1,54 @@
 ﻿Console.Write("Ingrese una ruta: ");
 string path = Console.ReadLine()!;
+string archivoCSV = Directory.GetCurrentDirectory() + "\\index.csv";
 
-mostrarDirectorio(path);
+if ( !File.Exists(archivoCSV) ) {
+  File.Create(archivoCSV);
+};
 
-void mostrarDirectorio(string path) {
+mostrarDirectorioYGuardarContenido(path);
+
+void mostrarDirectorioYGuardarContenido(string path) {
 
   if (Directory.Exists(path)) {
+
+    StreamWriter sr = new StreamWriter(archivoCSV);
+
       
     List<string> listadoCarpetas = Directory.GetDirectories(path).ToList();
+
+    Console.WriteLine("-----Carpetas-----");
+
+    int aux = 1;
 
     foreach (string carpeta in listadoCarpetas) {
 
       string[] carpetaAMostrar = carpeta.Split("\\");
-
       Console.WriteLine("/" + carpetaAMostrar[carpetaAMostrar.Length - 1]);
+
+      sr.WriteLine($"{ aux },{ carpetaAMostrar[carpetaAMostrar.Length - 1] }");
+
+      aux++;
     };
 
-    foreach (string carpeta in Directory.GetFiles(path)) {
+    Console.WriteLine("-----Archivos-----");
+
+    foreach (string archivo in Directory.GetFiles(path)) {
       
-      string[] carpetaAMostrar = carpeta.Split("\\");
+      string[] archivoAMostrar = archivo.Split("\\");
+      string[] archivoArray = archivoAMostrar[archivoAMostrar.Length - 1].Split(".");
+      string nombreArchivo = archivoArray[0];
+      string extensionArchivo = archivoArray[archivoArray.Length - 1];
 
-      Console.WriteLine(carpetaAMostrar[carpetaAMostrar.Length - 1]);
+      Console.WriteLine(archivoAMostrar[archivoAMostrar.Length - 1]);
+
+      sr.WriteLine($"{ aux },{ nombreArchivo },{ extensionArchivo }");
+
+      aux++;
     };
+
+    sr.Close();
+  } else {
+    Console.WriteLine("El directorio no existe");
   };
 };
